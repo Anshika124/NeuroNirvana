2ba16f850ef13b85821f86277ed74e7f68e1008c
Chahat Srivastava <chahatsrivastava019@gmail.com>
2024-05-12 04:20:02 +0530

    Report component added

--- client/src/Report.js
+++ client/src/Report.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
-function Report() {
+import './Home.css'
+import './Report.css'
+import Navigation from './Navigation'
+const Report = () => {
   return (
-    <div>Report</div>
+    <>
+        
+                    <section class="rightSection">
+                        <div class="mainContainer">
+                            <div class="header-mainContainer">
+                                
+                                <h4>REPORT</h4>
+                            </div>
+                            <div class="main-mainContainer">
+                            <div class="report-text">
+                                
+                            </div>
+                            </div>
+                        </div>
+                    </section>
+                
+    </>
   )
 }
 
